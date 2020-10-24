f7294884206824eb3d207e3c06d3f5e2dfb5999a
ireina7 <comcx.yang@gmail.com>
2020-10-25 00:20:26 +0800

    zero

---
+++ Scala/two-sum.scala
@@ -0,0 +1,12 @@
+object Solution {
+    // Bad, but pure for comprehension!
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        val dict = scala.collection.mutable.Map[Int, Int]()
+        for {
+            pair@(x, i) <- nums.zipWithIndex
+            if dict.contains(target - x) || { dict += pair; false }
+            j <- dict.get(target-x)
+        } return Array(j, i)
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
